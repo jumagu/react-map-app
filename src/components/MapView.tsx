1603b767984b0b7ab1755ce2c0fefc3afba6d94e
jumagu <jmgg.gutierrez@gmail.com>
2024-06-10 22:20:24 -0500

    feat: Update MapView to dynamically set map style based on theme

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -3,9 +3,15 @@ import { useContext, useLayoutEffect, useRef } from "react";
 import { Map } from "mapbox-gl";
 
 import { Loading } from "./Loading";
-import { MapContext, PlacesContext, UiContext } from "../contexts";
+import {
+  UiContext,
+  MapContext,
+  ThemeContext,
+  PlacesContext,
+} from "../contexts";
 
 export const MapView = () => {
+  const { theme } = useContext(ThemeContext);
   const { handleResultsBoxVisibility } = useContext(UiContext);
   const { setMap } = useContext(MapContext);
   const { isLoading, userLocation } = useContext(PlacesContext);
@@ -16,14 +22,14 @@ export const MapView = () => {
     if (!isLoading && userLocation) {
       const map = new Map({
         container: mapDiv.current!,
-        style: "mapbox://styles/mapbox/dark-v11",
+        style: `mapbox://styles/mapbox/${theme}-v11`,
         center: userLocation,
         zoom: 9,
       });
 
       setMap(map);
     }
-  }, [isLoading, userLocation]);
+  }, [isLoading]);
 
   if (isLoading)
     return (
